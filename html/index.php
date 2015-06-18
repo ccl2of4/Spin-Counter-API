64f504afa60b890ef3de4dd1b598a87e41f88785
root <ccl2of4@tx.rr.com>
2015-06-18 20:47:10 +0000

    added output of phpdoc

---
+++ html/index.php
@@ -0,0 +1,126 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Spin Counter</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="styles.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Spin Counter</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="leaderboard.html">Leaderboard</a></li>
+            <li><a href="links.html">Links</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>    
+
+    <div class="container main">
+      <div class="row padded_vertical">
+        <div class="col-xs-1"></div>
+        <div class="col-xs-1">
+          <img src="images/b_brawlbg_l.png" class="img-responsive center-block">
+        </div>
+        <div class="col-xs-8">
+          <img src="images/logo.png" class="img-responsive center-block">
+        </div>
+        <div class="col-xs-1">
+          <img src="images/brawlbg.png" class="img-responsive center-block">
+        </div>
+        <div class="col-xs-1"></div>
+      </div>
+
+      <div class="row padded_horizontal">
+        <h2>Description</h2>
+        <p class="lead">
+          Spin Counter is an app where users compete to see who's the best at spinning their phone. It's inspired by games such as Flappy Bird that are addictive because of their low learning curves, short turn lengths, and frustration.
+        </p>
+      </div>
+
+      <div class="row padded_horizontal">
+        <h2>Features</h2>
+        <ul>
+          <li>
+            Single Player Mode
+          </li>
+          <li>
+            2 Player Bluetooth Brawl
+          </li>
+          <li>
+            Global Rankings
+          </li>
+          <li>
+            Friends List
+          </li>
+        </ul>
+      </div>
+
+      <div class="row padded_horizontal">
+        <h2>Screenshots</h2>
+        <div class="col-xs-1 padded_vertical"></div>
+        <div class="col-xs-2 padded_vertical">
+          <img src="images/screen_1.jpg" class="img-responsive img-rounded center-block">
+        </div>
+        <div class="col-xs-2 padded_vertical">
+          <img src="images/screen_2.jpg" class="img-responsive img-rounded center-block">
+        </div>
+        <div class="col-xs-2 padded_vertical">
+          <img src="images/screen_3.jpg" class="img-responsive img-rounded center-block">
+        </div>
+        <div class="col-xs-2 padded_vertical">
+          <img src="images/screen_4.jpg" class="img-responsive img-rounded center-block">
+        </div>
+        <div class="col-xs-2 padded_vertical">
+          <img src="images/screen_5.jpg" class="img-responsive img-rounded center-block">
+        </div>
+        <div class="col-xs-1 padded_vertical"></div>
+      </div>
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
